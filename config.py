9bd64b6eea3f563d180bb42f464aa2e01fafb023
Prabhas <118895531+Anupamafan@users.noreply.github.com>
2024-04-03 21:09:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ DB_NAME = environ.get("DB_NAME", "cluster01")
 # Bot Information
 BOT_TOKEN = environ.get("BOT_TOKEN", "6703353886:AAHCoUV7PmNQWyJ23PfdTc5gMJNYgRNuHHg")
 BOT_USERNAME = environ.get("BOT_USERNAME", "Movies_Telugu_Top_Bot") # your bot username without @
-PICS = (environ.get('PICS', 'https://graph.org/file/82ef767ffebe3a948e476.jpg https://graph.org/file/82ef767ffebe3a948e476.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://graph.org/Movies-Telugu-Top-04-03')).split() # Bot Start Picture
 
 # Auto Delete Information
 AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
